57f32ca60bedfedae5d41275e23bb45e10635174
Ioann Kurchin <i.kurchin@bilderlings.eu>
2020-12-26 03:20:54 +0200

    Adding header component and links to home, shop page and contacts

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import {Route, Switch} from "react-router";
+
 import HomePage from "./pages/home/HomePage"
 import ShopPage from "./pages/shop/ShopPage";
+import Header from "./components/header/Header";
 
 import './App.css';
-
 
 function App() {
     return (
         <div className="App">
+            <Header />
             <Switch>
                 <Route exact path='/' component={HomePage}/>
                 <Route path='/shop' component={ShopPage} />
